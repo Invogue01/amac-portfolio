bd3f25a927254361b6b192fb9e634ab444aed603
Andrea Mac <aleatrengrouse@gmail.com>
2021-12-15 21:30:44 +0000

    added script for icons and hamburger menu styling

---
+++ assets/app.js
@@ -0,0 +1,7 @@
+const hamurgerButton = document.getElementById('hamburger')
+const navList = document.getElementById('nav-list')
+
+function toggleButton() {
+    navList.classList.toggle('show')
+}
+hamurgerButton.addEventListener('click', toggleButton)
